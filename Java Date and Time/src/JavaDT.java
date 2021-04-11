fdbb7cba9b89f6297d3e33042786256849cca4b8
Berke <76871439+Berkec98@users.noreply.github.com>
2021-04-12 02:25:39 +0300

    LocalDate - ChronoField

--- Java Date and Time/src/JavaDT.java
+++ Java Date and Time/src/JavaDT.java
@@ -67,3 +67,41 @@ public class JavaDT {
     }
 }
 
+
+
+
+// Without Function find the day
+
+import java.time.LocalDate;  
+import java.time.temporal.ChronoField;  
+public class LocalDateTimeExample {  
+  public static void main(String[] args) {  
+    LocalDate a = LocalDate.of(2017, 2, 21);   
+    int b= a.get(ChronoField.DAY_OF_WEEK); 
+   switch(b) {
+   case 1:
+    System.out.println("Monday");
+    break;
+  case 2:
+    System.out.println("Tuesday");
+    break;
+  case 3:
+    System.out.println("Wednesday");
+    break;
+  case 4:
+    System.out.println("Thursday");
+    break;
+  case 5:
+    System.out.println("Friday");
+    break;
+  case 6:
+    System.out.println("Saturday");
+    break;
+  case 7:
+    System.out.println("Sunday");
+    break;
+}
+  }  
+}
+
+
